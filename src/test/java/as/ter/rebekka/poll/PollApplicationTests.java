0416148a2b6f0ff33ebdd9740bb9e5bc956e44e7
rebekkabb <breedis.rebekka@gmail.com>
2020-01-21 20:23:35 +0200

    initial commit

---
+++ src/test/java/as/ter/rebekka/poll/PollApplicationTests.java
@@ -0,0 +1,13 @@
+package as.ter.rebekka.poll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

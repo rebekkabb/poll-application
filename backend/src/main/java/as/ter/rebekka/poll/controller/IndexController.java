bb5763a150e07b205db14bd67af73d0d3969173b
rebekkabb <breedis.rebekka@gmail.com>
2020-01-23 23:29:30 +0200

    Removed mustache, removed redundant files and simplified index controller

--- backend/src/main/java/as/ter/rebekka/poll/controller/IndexController.java
+++ backend/src/main/java/as/ter/rebekka/poll/controller/IndexController.java
@@ -1,19 +1,14 @@
 package as.ter.rebekka.poll.controller;
 
-import as.ter.rebekka.poll.route.DefaultRoute;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class IndexController {
-    @Autowired
-    private DefaultRoute defaultRoute;
-
     @RequestMapping("/")
     public ModelAndView getDefault() {
-        return new ModelAndView("index", defaultRoute.getDefault());
+        return new ModelAndView("index.html");
     }
 
     @RequestMapping(value = "/**/{[path:[^.]*}")

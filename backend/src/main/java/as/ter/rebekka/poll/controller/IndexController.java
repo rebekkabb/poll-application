4dabe0baa5bc7682774a35f95c2514fa9f65f7ce
rebekkabb <breedis.rebekka@gmail.com>
2020-01-23 23:20:17 +0200

    Fixed frontend routing in backend

--- backend/src/main/java/as/ter/rebekka/poll/controller/IndexController.java
+++ backend/src/main/java/as/ter/rebekka/poll/controller/IndexController.java
@@ -2,24 +2,23 @@ package as.ter.rebekka.poll.controller;
 
 import as.ter.rebekka.poll.route.DefaultRoute;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/")
 public class IndexController {
     @Autowired
     private DefaultRoute defaultRoute;
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public ModelAndView getDefault() {
         return new ModelAndView("index", defaultRoute.getDefault());
     }
 
-    @GetMapping("/create")
-    public ModelAndView getCreate() {
-        return new ModelAndView("create", defaultRoute.getCreate());
+    @RequestMapping(value = "/**/{[path:[^.]*}")
+    public ModelAndView redirect() {
+        // Forward to home page so that route is preserved.
+        return getDefault();
     }
 }

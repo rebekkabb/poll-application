c34d261d2fb4e877f94d4f14f2023cf776494085
rebekkabb <breedis.rebekka@gmail.com>
2020-01-24 12:20:05 +0200

    Can remove options close #2

--- frontend/src/components/CreatePoll.vue
+++ frontend/src/components/CreatePoll.vue
@@ -5,23 +5,27 @@
             Title
             <input type="text" v-model="poll.title"/>
         </label>
-        <label>
-            Multiple answers
-            <input type="checkbox" v-model="poll.multipleAnswers"/>
-        </label>
-        <label>
-            Duplication check
-            <input type="checkbox" v-model="poll.duplicationCheck">
-        </label>
+        <br>
         <div v-for="(option, index) in poll.options">
             <label>
                 Option {{index + 1}}
                 <input type="text" v-model="poll.options[index]">
+                <button @click="deleteOption(index)">Delete option</button>
             </label>
         </div>
         <button @click="addOption">Add option</button>
         <button @click="sendPoll">Send poll</button>
         {{this.poll}}
+        <br>
+        <label>
+            Multiple answers
+            <input type="checkbox" v-model="poll.multipleAnswers"/>
+        </label>
+        <br>
+        <label>
+            Duplication check
+            <input type="checkbox" v-model="poll.duplicationCheck">
+        </label>
     </div>
 </template>
 
@@ -38,6 +42,9 @@
             this.poll.options.push("")
         }
 
+        private deleteOption(index: number): void {
+            this.$delete(this.poll.options, index);
+        }
 
         private sendPoll(): void {
             axios.put("/api/poll", this.poll).then((res) => {

6be5c4f125ed3344af7b7b3c0862170c0d711ee6
rebekkabb <breedis.rebekka@gmail.com>
2020-02-10 01:48:47 +0200

    Added support for images in front-end

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,10 +1,16 @@
 <template>
-    <div class="jumboboy">
-        <div class="bootstrap">
-            <b-jumbotron header="Poll application" lead="Make ur own polls!" border-variant="dark">
-                <b-button variant="outline-primary" to="/polls">Go to Polls</b-button>
-            </b-jumbotron>
+    <div>
+        <div class="colr">
+            <div class="bootstrap" id="jumboboy">
+                <b-jumbotron header="Poll application" lead="Make ur own polls!" border-variant="dark">
+                    <b-button variant="outline-primary" to="/polls">Go to Polls</b-button>
+                </b-jumbotron>
+            </div>
+            <div class="separator">
+                <img src=../public/img/white-curve.png>
+            </div>
         </div>
+
     </div>
 </template>
 <script lang="ts">
@@ -18,10 +24,22 @@
 
 
 <style>
-    .jumboboy {
+    #jumboboy {
         width: 50%;
         margin-top: 15%;
         margin-left: 25%;
         text-align: center;
+
+    }
+
+    .separator {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(black, pink);
+    }
+
+    html {
+        height: 100%;
     }
 </style>
